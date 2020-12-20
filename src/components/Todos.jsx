9219664b83821ae70a30426b6b1f4874f586103b
ahmatrizal <65964090+ahmatrizal@users.noreply.github.com>
2020-12-20 23:43:48 +0700

    penambahan pages

--- src/components/Todos.jsx
+++ src/components/Todos.jsx
@@ -1,12 +1,24 @@
 import React from "react";
 import PropTypes from "prop-types";
+import Todo from "./Todo";
 
 const Todos = () => {
+  const todos = [
+    {
+      text: "belajar react"
+    },
+    {
+      text: "belajar react"
+    },
+    {
+      text: "belajar react"
+    }
+  ];
   return (
     <section className="todos">
-      <div className="todo">
-        <span className="todo-text">Learning React</span>
-      </div>
+      {todos.map((todo) => {
+        return <Todo text={todo.text} />;
+      })}
     </section>
   );
 };
